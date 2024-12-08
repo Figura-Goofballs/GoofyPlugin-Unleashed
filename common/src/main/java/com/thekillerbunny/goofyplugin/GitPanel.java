ba3db958db16d0cb035dbc5b3cd1f38e266ffbe9
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-12-08 16:39:33 -0500

    feat(git): commits list

--- common/src/main/java/com/thekillerbunny/goofyplugin/GitPanel.java
+++ common/src/main/java/com/thekillerbunny/goofyplugin/GitPanel.java
@@ -3,9 +3,16 @@ package com.thekillerbunny.goofyplugin;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import org.figuramc.figura.gui.screens.AbstractPanelScreen;
+import org.figuramc.figura.gui.widgets.lists.AbstractList;
 
 public class GitPanel extends AbstractPanelScreen {
     public GitPanel(Screen parent) {
         super(parent, Component.translatable("goofyplugin.gui.panels.title.history"));
+    }
+
+    public static class Commits extends AbstractList {
+        public Commits(int x, int y, int width, int height) {
+            super(x, y, width, height);
+        }
     }
 }
